b9710620c93bad909544d552c694282a59ea4098
Diego Souza <dee-souza@hotmail.com>
2021-10-25 10:59:25 -0300

    chore: add comments

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,5 +1,6 @@
-import { GraphQLServer } from "graphql-yoga";
 import path from "path";
+
+import { GraphQLServer } from "graphql-yoga";
 import mongoose from "mongoose";
 
 import mongoConfig from "./config/mongo";
@@ -7,6 +8,7 @@ import resolvers from "./resolvers";
 
 const { log, error } = console;
 
+// Conexão com o MondoDB
 const MONGO_URL = `mongodb://${mongoConfig.host}:${mongoConfig.port}/${mongoConfig.base}`;
 
 mongoose
@@ -17,7 +19,18 @@ mongoose
   .then(() => log(`MongoDB Conectado: ${MONGO_URL}`))
   .catch((err) => error(err));
 
+// Caminho do arquivo de schema
 const typeDefs = path.resolve("./src/schema.graphql");
-const server = new GraphQLServer({ typeDefs, resolvers });
 
-server.start(() => log("Server is running on http://localhost:4000"));
+/**
+ * @param typeDefs Aplicação do Schema do GraphQL com as definições de tipos
+ * @param resolvers Responsável pela execução das queries definidas no schema da aplicação.
+ * Os dados utilizados em uma aplicação que utiliza GraphQL pode ter origem em um banco de dados...
+ * ... ou até mesmo em um array de objetos.
+ */
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(() => log("Server GraphQL is running on http://localhost:4000"));
