ca6265c449054d240955df3f534ee99f85068630
Diego Souza <dee-souza@hotmail.com>
2022-05-13 15:34:26 -0300

    change color icon button when showMenu is open

--- frontend/src/organisms/MenuButton/index.tsx
+++ frontend/src/organisms/MenuButton/index.tsx
@@ -1,13 +1,16 @@
-import { useMenu } from "contexts/MainContext";
 import { FaGamepad } from "react-icons/fa";
+
+import { useMenu } from "contexts/MainContext";
 import { Button } from "./styles";
 
 const MenuButton = () => {
-  const { handleToggleMenu } = useMenu();
+  const { handleToggleMenu, showMenu } = useMenu();
+
+  const colorIcon = showMenu ? "#20b2aa" : "#ff6347";
 
   return (
     <Button onClick={handleToggleMenu}>
-      <FaGamepad size={32} color="#ff6347" />
+      <FaGamepad size={32} color={colorIcon} />
     </Button>
   );
 };
--- frontend/src/organisms/MenuButton/styles.ts
+++ frontend/src/organisms/MenuButton/styles.ts
@@ -3,4 +3,8 @@ import styled from "styled-components";
 export const Button = styled.button`
   background: none;
   border: 0;
+
+  svg {
+    transition: all 0.25s ease-in-out;
+  }
 `;
